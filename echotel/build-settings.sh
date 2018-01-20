f029fa241a158d0259485b0264790e17da53dfe1
Jose San Leandro <jose.sanleandro@osoco.es>
2018-01-21 00:59:42 +0100

    WIP

---
+++ echotel/build-settings.sh
@@ -0,0 +1,7 @@
+defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "0.9.22";
+overrideEnvVar TAG "v1.0.0";
+defineEnvVar SERVICE_USER "The echotel service user" "echotel";
+defineEnvVar SERVICE_USER_PASSWORD "The echotel service password" "secret";
+defineEnvVar SERVICE_GROUP "The echotel service group" "echotel";
+defineEnvVar SERVICE_USER_SHELL "The echotel account shell" "/bin/bash";
+defineEnvVar SERVICE_USER_HOME "The home of the echotel account" "/opt/cleanscore-api";
