c5bf3d2fda75653ffd4c472d35990ec4085944e5
Jose San Leandro <jose.sanleandro@osoco.es>
2018-01-21 12:56:49 +0100

    WIP

--- certify/build-settings.sh
+++ certify/build-settings.sh
@@ -1,5 +1,5 @@
 defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "0.9.22";
-overrideEnvVar TAG "1.0.0";
+overrideEnvVar TAG "1.0.1";
 defineEnvVar SERVICE_USER "The echotel service user" "certify";
 defineEnvVar SERVICE_USER_PASSWORD "The certify password" "secret";
 defineEnvVar SERVICE_GROUP "The certify service group" "certify";
