5985d3243551c1a693c70c6982d4929c2899094f
Jose San Leandro <jose.sanleandro@osoco.es>
2018-01-21 03:09:37 +0100

    Copied from ConsultancyPage

--- rest/app.js
+++ rest/app.js
@@ -8,6 +8,11 @@ var express = require("express"),
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var greenCoinAbi = require("../build/contracts/GreenScore.json");
 
